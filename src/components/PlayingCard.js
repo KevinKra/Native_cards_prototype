b34d5079ec844797b128a8f81f04070a5f190219
Turing School <kratok08@gmail.com>
2019-09-02 12:59:28 -0600

    Create basic stylesheet for card

--- src/components/PlayingCard.js
+++ src/components/PlayingCard.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import { Text, View, Animated, PanResponder } from "react-native";
+import { Text, View, Animated, PanResponder, StyleSheet } from "react-native";
 
 export default class PlayingCard extends Component {
   render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    );
+    return <View style={styles.card}></View>;
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    borderColor: "black",
+    borderWidth: 3,
+    borderRadius: 5,
+    height: 220,
+    width: 140,
+    backgroundColor: "#555"
+  }
+});
